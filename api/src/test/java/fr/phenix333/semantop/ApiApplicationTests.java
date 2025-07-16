982b18e3372961b1f71d44ff5c5ff39d1f15f52f
Colin-de-Seroux <77831261+Colin-de-Seroux@users.noreply.github.com>
2025-07-16 20:02:25 +0200

    :tada: Init API project

---
+++ api/src/test/java/fr/phenix333/semantop/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.phenix333.semantop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
