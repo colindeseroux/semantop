13e22e806d35388b5b0179587954e42646f2bd3f
Colin-de-Seroux <77831261+Colin-de-Seroux@users.noreply.github.com>
2025-08-13 20:35:40 +0200

    :truck: Change the type of new password route
    @PutMapping("/api/auth") -> legacy @PatchMapping("/api/auth/new-password")

--- api/src/main/java/fr/phenix333/semantop/controller/auth/AuthController.java
+++ api/src/main/java/fr/phenix333/semantop/controller/auth/AuthController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -107,7 +106,7 @@ public class AuthController {
 	 * 
 	 * @return ResponseEntity<String> -> "OK"
 	 */
-	@PutMapping
+	@PatchMapping("/new-password")
 	public ResponseEntity<String> newPassword(@RequestParam String email, @RequestParam String code,
 			@RequestParam String password, HttpServletRequest request) throws Exception {
 		L.function("email : {}", email);
